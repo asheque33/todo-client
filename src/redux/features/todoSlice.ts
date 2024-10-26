0b3c767058102440b8580a0f4c355b6d2f81b5f9
asheque33 <ashequrrahman3333@gmail.com>
2024-10-26 15:05:58 +0600

    include updateTodo & refactore others

--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -2,17 +2,17 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit/react";
 import { RootState } from "../store";
 
 type TTodo = {
-  id: string;
+  _id: string;
   title: string;
   priority: string;
   description: string;
   isCompleted?: boolean;
 };
 type TInitialState = {
-  todo: TTodo[];
+  todos: TTodo[];
 };
 const initialState: TInitialState = {
-  todo: [],
+  todos: [],
 };
 export const todoSlice = createSlice({
   name: "todo",
@@ -20,27 +20,39 @@ export const todoSlice = createSlice({
   reducers: {
     createTodo: (state, action: PayloadAction<TTodo>) => {
       // state.todo.push(action.payload);
-      state.todo.push({ ...action.payload, isCompleted: false });
-      console.log(state.todo.length - 1);
+      state.todos.push({ ...action.payload, isCompleted: false });
+      // console.log(state.todo.length - 1);
     },
-    removeTodo: (state, action: PayloadAction<string>) => {
-      state.todo = state.todo.filter((item) => item.id !== action.payload);
-      console.log(state.todo.length - 1);
+    updateTodo: (state, action: PayloadAction<TTodo>) => {
+      const indexNum = state.todos.findIndex(
+        (item) => item._id === action.payload._id
+      );
+      if (indexNum !== -1) {
+        state.todos[indexNum] = { ...state.todos[indexNum], ...action.payload };
+      } else {
+        console.log("Item not found", action.payload._id);
+        return;
+      }
+      // console.log(state.todo.length - 1);
     },
     toggleCompleted: (state, action: PayloadAction<string>) => {
-      const indexNum = state.todo.findIndex(
-        (item) => item.id === action.payload
+      const indexNum = state.todos.findIndex(
+        (item) => item._id === action.payload
       );
-      state.todo[indexNum].isCompleted = !state.todo[indexNum].isCompleted;
-      if (state.todo[indexNum].isCompleted) {
-        const done = state.todo[indexNum];
-        state.todo.splice(indexNum, 1);
-        state.todo.push(done);
+      state.todos[indexNum].isCompleted = !state.todos[indexNum].isCompleted;
+      if (state.todos[indexNum].isCompleted) {
+        const done = state.todos[indexNum];
+        state.todos.splice(indexNum, 1);
+        state.todos.push(done);
       }
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((item) => item._id !== action.payload);
+      // console.log(state.todo.length - 1);
     },
   },
 });
-export const { createTodo, removeTodo, toggleCompleted } = todoSlice.actions;
-export const selectTodo = (state: RootState) => state.todos.todo;
-
-export default todoSlice.reducer;
+export const selectedTodos = (state: RootState) => state.todos.todos;
+export const { createTodo, updateTodo, removeTodo, toggleCompleted } =
+  todoSlice.actions;
+export const todoReducer = todoSlice.reducer;
